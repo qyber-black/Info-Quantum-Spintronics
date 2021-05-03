23d3c1610509cf0bfe27c1d5f86abab85ce26101
Frank C Langbein <frank@langbein.org>
2021-05-03 12:42:21 +0100

    Clone via https.

--- mirror.sh
+++ mirror.sh
@@ -3,11 +3,11 @@
 set -e
 
 echo "# Cloning github wiki"
-git clone git@github.com:xis10z/Info-Quantum-Spintronics.wiki.git
+git clone https://github.com/xis10z/Info-Quantum-Spintronics.wiki.git
 cd Info-Quantum-Spintronics.wiki
 
 echo "# Rebasing qyber wiki"
-git remote add qyber git@qyber.black:quantum-spintronics/info-quantum-spintronics.wiki.git
+git remote add qyber https://qyber.black/quantum-spintronics/info-quantum-spintronics.wiki.git
 git fetch qyber master
 git rebase qyber/master
 
