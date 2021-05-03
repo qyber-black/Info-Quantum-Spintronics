b6e9853d2bccdfe1999deb2e49e81f7473cce651
Frank C Langbein <frank@langbein.org>
2021-05-03 13:28:28 +0100

    Prettier output.

--- mirror.sh
+++ mirror.sh
@@ -7,11 +7,13 @@ if [ -z "${WIKI_PERSONAL_ACCESS_TOKEN}" ]; then
   exit 1
 fi
 
+echo
 echo "# Cloning github wiki"
 git clone "https://${WIKI_PERSONAL_ACCESS_TOKEN}@${GITHUB_SERVER_URL#https://}/$GITHUB_REPOSITORY.wiki.git"
 wiki_dir="`basename $GITHUB_REPOSITORY.wiki`"
 
-echo "# Rebasing qyber wiki"
+echo
+echo "# Sync'ing with qyber wiki"
 cd "$wiki_dir"
 git remote add qyber https://qyber.black/quantum-spintronics/info-quantum-spintronics.wiki.git
 git config --global user.email "frank@langbein.org"
@@ -22,9 +24,10 @@ git rebase qyber/master
 git pull
 cd ..
 
+echo
 echo "# Update paper"
-curl https://d.qyber.black/paper/quantum-spintronics-paper-ingaas-spin-transport/paper.pdf --output paper.pdf
-if ! diff paper.pdf "$wiki_dir/paper/quantum-spintronics-paper-ingaas-spin-transport.pdf"; then
+curl -s https://d.qyber.black/paper/quantum-spintronics-paper-ingaas-spin-transport/paper.pdf --output paper.pdf
+if ! diff -q paper.pdf "$wiki_dir/paper/quantum-spintronics-paper-ingaas-spin-transport.pdf"; then
   echo "## PDFs are different"
   cd "$wiki_dir"
   mv ../paper.pdf paper/quantum-spintronics-paper-ingaas-spin-transport.pdf
@@ -33,6 +36,7 @@ if ! diff paper.pdf "$wiki_dir/paper/quantum-spintronics-paper-ingaas-spin-trans
   cd ..
 fi
 
+echo
 echo "# Update wiki"
 cd "$wiki_dir"
 git push
